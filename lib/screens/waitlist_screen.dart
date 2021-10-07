f37fc00f1d746993e1c4e6b6ab9ef20b8d38ce59
siliconvallii <filippovalli2003@gmail.com>
2021-10-07 18:06:54 +0200

    fix WaitlistScreen styling

--- lib/screens/waitlist_screen.dart
+++ lib/screens/waitlist_screen.dart
@@ -1,17 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:writeai/widgets/discover_more.dart';
 
 class WaitlistScreen extends StatelessWidget {
   const WaitlistScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Waitlist'),
-      ),
-      body: const SafeArea(
-        child: Center(
-          child: Text('You are waitlisted'),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xffffffff),
+          elevation: 0,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width / 100 * 90,
+              child: Column(
+                children: [
+                  const Spacer(flex: 10),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 100 * 10,
+                    child: Image.asset('assets/images/logo-removebg.png'),
+                  ),
+                  const Spacer(flex: 40),
+                  const Text(
+                    'Thanks for signing up!  🎉',
+                    style: TextStyle(
+                      fontFamily: 'Lato',
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(flex: 20),
+                  const Text(
+                    'We will send you an email when your account'
+                    ' is ready.\n\nIf you’ve signed up for our'
+                    ' newsletter, you will also be getting updates'
+                    ' from us as we publish them.'
+                    ' If you wish to subscribe to our list,'
+                    ' you can do that from our website.'
+                    '\n\nWe can’t wait to write with you soon!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Lato', fontSize: 16, height: 1.6),
+                  ),
+                  const Spacer(flex: 15),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    width: MediaQuery.of(context).size.width / 100 * 80,
+                    child: const Text(
+                      '-Your team at Write AI',
+                      style: TextStyle(
+                        fontFamily: 'Lato',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                  const Spacer(flex: 100),
+                  const DiscoverMore(),
+                  const Spacer(flex: 20),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
