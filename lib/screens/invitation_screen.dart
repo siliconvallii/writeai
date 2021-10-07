a3011b7bf32519d6a06b3d59702caed125c70db6
siliconvallii <filippovalli2003@gmail.com>
2021-10-07 18:26:29 +0200

    fix styling in InvitationScreen

--- lib/screens/invitation_screen.dart
+++ lib/screens/invitation_screen.dart
@@ -1,32 +1,67 @@
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:writeai/data/data.dart';
+import 'package:writeai/widgets/bottom_button.dart';
 
 class InvitationScreen extends StatelessWidget {
   const InvitationScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // instantiate Realtime Database reference
-    final DatabaseReference ref = FirebaseDatabase.instance.reference();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Invitation'),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              const Text('You have been invited'),
-              ElevatedButton(
-                child: const Text('Continue'),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/form');
-                },
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xffffffff),
+          elevation: 0,
+          centerTitle: true,
+          title: SizedBox(
+            height: MediaQuery.of(context).size.height / 100 * 4,
+            child: Image.asset('assets/images/logo-removebg.png'),
+          ),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width / 100 * 90,
+              child: Column(
+                children: [
+                  const Spacer(flex: 20),
+                  const Text(
+                    'Your Write AI invitation',
+                    style: TextStyle(
+                      color: Color(0xff000000),
+                      fontFamily: 'Lato',
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(flex: 30),
+                  const Text(
+                    'Hello Mads,'
+                    '\n\nThank you again for using our app'
+                    ' to sign up for the Write AI waitlist.'
+                    '\n\nWe\'re thrilled to inform you'
+                    ' that your Write AI application has'
+                    ' been approved! You can use the'
+                    ' button below to finish creating'
+                    ' your account and start using Write AI.',
+                    style: TextStyle(
+                      color: Color(0xff000000),
+                      fontFamily: 'Lato',
+                      fontSize: 16,
+                      height: 1.6,
+                    ),
+                  ),
+                  const Spacer(flex: 100),
+                  BottomButton(
+                    title: 'Finish Sign In',
+                    onPressed: () => Navigator.pushNamed(context, '/form'),
+                  ),
+                  const Spacer(
+                    flex: 10,
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
