33b5e9f4e321d0be764d1672745612b590be78f6
siliconvallii <filippovalli2003@gmail.com>
2021-10-07 17:09:06 +0200

    fix InitialScreen styling

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,6 +1,8 @@
 // packages
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+// widgets
+import 'package:writeai/widgets/bottom_button.dart';
+import 'package:writeai/widgets/custom_header.dart';
 
 class InitialScreen extends StatelessWidget {
   const InitialScreen({Key? key}) : super(key: key);
@@ -9,18 +11,47 @@ class InitialScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Initial'),
+        backgroundColor: const Color(0xffffffff),
+        elevation: 0,
       ),
       body: SafeArea(
         child: Center(
           child: Column(
             children: [
-              ElevatedButton(
-                child: const Text('Join Now'),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/about_us');
-                },
+              const Spacer(flex: 10),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 100 * 25,
+                child: Image.asset('assets/images/logo-removebg.png'),
               ),
+              const Spacer(flex: 40),
+              const CustomHeader(title: 'WRITE AI'),
+              const Spacer(),
+              Container(
+                height: 1,
+                width: MediaQuery.of(context).size.width / 100 * 75,
+                color: const Color(0xff000000),
+              ),
+              const Spacer(flex: 5),
+              const Text(
+                'enhance your writing with AI',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 24,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              const Spacer(flex: 100),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 100 * 80,
+                height: MediaQuery.of(context).size.height / 100 * 5,
+                child: BottomButton(
+                  title: 'Join Now',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/about_us');
+                  },
+                ),
+              ),
+              const Spacer(flex: 15),
             ],
           ),
         ),
